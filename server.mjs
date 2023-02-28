3f31fee680c318fd36f3a1a930ab63f01480ec9f
Tayyaba-Ali20 <ta6402792@gmail.com>
2023-02-28 22:25:57 +0500

    more changes

--- server.mjs
+++ server.mjs
@@ -3,11 +3,8 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-let store = [];
-
 app.use(cors());
 app.get('/', (req, res) => {
-	store.push[req.body];
 	res.send('Hello tayyaba');
 });
 
